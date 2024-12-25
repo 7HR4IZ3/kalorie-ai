113db05d13b96a309b1da14a0904052e63cc52af
7HR4IZ3 <gamerxville@gmail.com>
2024-12-24 23:18:56 -0800

    fix: update vercel server handler

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const app = fastify({
   logger: true,
 });
 
+await setup(app, {});
+
 export default async function handler(req, res) {
   await app.ready();
   app.server.emit("request", req, res);
